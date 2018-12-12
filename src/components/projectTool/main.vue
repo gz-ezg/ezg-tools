c1c685cf1293cf22451d617434ccea87464912ee
roderickt1an <roderickt1an@gmail.com>
2018-12-12 17:47:33 +0800

    update project-page-show

--- src/components/projectTool/main.vue
+++ src/components/projectTool/main.vue
@@ -1,11 +1,11 @@
 <template>
-    <div style="background-color:#07BAE5;height:100vh;overflow-y:hidden">
-        <img src="./static/project.jpg" style="width:100%;height:70vh;margin-top:-15vh">
-        <div class="main" style="margin-top:-15px">
-            <div style="margin-top:10px!important;width:80%;margin:auto">
-                <field style="margin-bottom:5px;border-radius:6px;margin-top:5px;padding-left:10px;" :value="companyname" @input="update_companyname" placeholder="公司名称"></field>
-                <field style="margin-bottom:5px;border-radius:6px;padding-left:10px;" :value="name" @input="update_name" placeholder="负责人名称"></field>
-                <field style="margin-bottom:5px;border-radius:6px;padding-left:10px;" :value="tel" @input="update_tel" placeholder="联系方式"></field>
+    <div style="background-color:#07BAE5;height:100vh;overflow-y:hidden;" class="content">
+        <!-- <img src="./static/project.jpg" style="width:100%;height:70vh;margin-top:-15vh"> -->
+        <div class="main" style="margin-top:52vh">
+            <div style="margin-top:10px!important;width:80%;margin:auto" id="cell">
+                <field style="margin-bottom:5px;border-radius:10px;margin-top:5px;padding-left:10px;" :value="companyname" @input="update_companyname" placeholder="公司名称"></field>
+                <field style="margin-bottom:5px;border-radius:10px;padding-left:10px;" :value="name" @input="update_name" placeholder="负责人名称"></field>
+                <field style="margin-bottom:5px;border-radius:10px;padding-left:10px;" :value="tel" @input="update_tel" placeholder="联系方式"></field>
                 <Button type="danger" size="large" @click="submit" :disabled="disabled" :loading="!loading" style="margin-top:5px"> 立刻提交</Button>
             </div>
         </div>
@@ -67,3 +67,17 @@ export default {
     }
 }
 </script>
+
+<style>
+.content{
+    background-image: url("./static/backgroundImage.png");
+    background-size:100vw 100vh;
+}
+#cell .van-cell{
+    background-color: #faadaa
+}
+#cell .van-button__text{
+    color: #383838;
+    font-weight: 900;
+}
+</style>
